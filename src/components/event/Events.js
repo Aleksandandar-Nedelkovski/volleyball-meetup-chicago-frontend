94daa9b23dab3a89e24a8a85185e510f797c3d6f
Aleksandandar-Nedelkovski <aleksn.dev@gmail.com>
2022-04-01 21:02:13 -0500

    event form cors errors

--- src/components/event/Events.js
+++ src/components/event/Events.js
@@ -1,25 +1,23 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { getEvents } from "../../actions/event"
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import EventItem from "./EventItem"
 import EventForm from "./EventForm"
 
-const Events = ( { getEvents, event: { events } }) => {
+const Events = ({ getEvents, event: { events } }) => {
   useEffect(() => {
     getEvents();
   }, [getEvents]);
-  
-  console.log(events)
+
   return (
     <section className="container">
       <h1 className="large">Events</h1>
-      
       <EventForm />
       <div className="events">
-        {/* {events.map((event) => (
+        {events ? (events.map((event) => (
           <EventItem key={event._id} event={event} />
-        ))} */}
+        ))) : (null)}
       </div>
     </section>
   )
